be510182ab6a2a458540eb62074397c2bd05f59c
Dean Wyatte <dean.wyatte@activision.com>
2019-04-09 14:42:11 -0600

    fix

--- example.sh
+++ example.sh
@@ -9,4 +9,5 @@ cd example_embeddings
 curl -o glove_embeddings.zip https://nlp.stanford.edu/data/glove.6B.zip
 unzip glove_embeddings.zip
 rm glove_embeddings.zip glove.6B.50d.txt glove.6B.300d.txt glove.6B.200d.txt
+cd ../..
 python example.py
